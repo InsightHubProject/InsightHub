3a6d1391d969c7023eafd579d1fb7efa51528d11
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-01-24 18:20:14 -0500

    Refactored routing in App.js and index.js, added Home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import hero_img from "../assets/logo.png";
+
+const Home = () => {
+  return (
+    // The padding is smaller when the screen is smal
+      <div className="hero py-12 sm:py-32 bg-base-300">
+        <div className="hero-content grid md:grid-cols-2 gap-8">
+          {/* The image for large screen */}
+          <div className="order-2 hidden md:block">
+            <img src={hero_img} alt="Hero" />
+          </div>
+
+          <div>
+            <h1 className="text-4xl sm:text-5xl font-bold leading-normal">
+                Welcome to InsightHub, Your Gateway to Social Media Insights!
+            </h1>
+
+            {/* The image for small screen */}
+            <div className="order-2 block md:hidden">
+              <img src={hero_img} alt="Hero" className="w-4/6 h-auto mx-auto" />
+            </div>
+
+            <p className="py-6 text-3xl leading-normal">
+                Analyze Trends with Ease, Understand Sentiments in a Breeze.
+            </p>
+
+            <Link to="/about" className="btn btn-warning w-1/2 text-xl">
+              About us
+            </Link>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Home;
