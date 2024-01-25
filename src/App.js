666be962f2ce6c98b6167954aedb4cec41f314a7
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-01-24 20:45:08 -0500

    Add new routes for Analysis and Reports pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import { Route, Routes } from "react-router-dom";
 
 import Home from './pages/Home';
+import Analysis from './pages/Analysis';
+import Reports from './pages/Reports';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/analysis" element={<Analysis />} />
+        <Route path="/reports" element={<Reports />} />
       </Routes>
     </>
   );
