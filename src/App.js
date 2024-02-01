bbfe049023f58b67d1df2d099ba64e3e51f9856a
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-02-01 10:12:31 -0500

    Update imports and add ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
-import Home from './pages/Home';
-import Analysis from './pages/Analysis';
-import Reports from './pages/Reports';
-import AboutUs from './pages/AboutUs';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import ContactUs from './pages/ContactUs';
+import Home from "./pages/Home";
+import Analysis from "./pages/Analysis";
+import Reports from "./pages/Reports";
+import AboutUs from "./pages/AboutUs";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/analysis" element={<Analysis />} />
@@ -21,6 +23,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
+      <ScrollToTop />
     </>
   );
 }
