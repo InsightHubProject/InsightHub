eea867391f13c635c201e2337ca4c9178c03f74d
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-03-18 17:16:27 -0400

    Used the new added data function

--- Python/src/main.py
+++ Python/src/main.py
@@ -2,17 +2,26 @@ from TweetFetcher import TweetFetcher
 from TweetAnalyzer import TweetAnalyzer
 
 def main():
+
+    word = "tesla"
+
     # Used to fetch tweets from the database
     fetcher = TweetFetcher()
-    tweets = fetcher.get_tweets('tesla')
+    tweets = fetcher.get_tweets(word)
 
     # Used to analyze the tweets
     analyzer = TweetAnalyzer()
     data = analyzer.analyze_tweets(tweets)
     data = analyzer.convert_date(data)
-    grouped_data = analyzer.group_data(data)
+    grouped_data = analyzer.group_data_table(data)
+    grouped_data_list = analyzer.group_data_list(data)
 
-    print(grouped_data)
+    print()
+    print(f"-----------------Table of {word} Tweets-----------------")
+    print(grouped_data, end="\n\n")
+
+    print(f"-----------------List of {word} Tweets-----------------")
+    print(grouped_data_list)
 
 if __name__ == "__main__":
     main()
