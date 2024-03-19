5f0e7d1245be83151236379bd5ede7838eb33238
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-03-19 17:43:30 -0400

    Add sentiment analysis charts and update dataset options

--- src/pages/Analysis.jsx
+++ src/pages/Analysis.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
 
 import Layout from "../components/Layout";
 
 import data1_sample from "../data/data1_sample.json";
 import data2_sample from "../data/data2_sample.json";
+
+import { SentimentStackedBarChart } from "../components/Charts/SentimentStackedBarChart";
+import { SentimentLineChart } from "../components/Charts/SentimentLineChart";
+import { SentimentPieCharts } from "../components/Charts/SentimentPieChart";
 
 const Analysis = () => {
   // Assume data_sample is the array you want to use directly
@@ -59,29 +54,13 @@ const Analysis = () => {
             <option value="" disabled selected>
               Choose Dataset
             </option>
-            <option value="1">Dataset 1</option>
-            <option value="2">Dataset 2</option>
+            <option value="1">Twitter</option>
+            <option disabled value="2">Facebook (Soon)</option>
           </select>
           <button className="btn btn-warning w-1/8 text-xl">
             Generate Report
           </button>
         </div>
-
-        {/* Display selected dataset content */}
-        {selectedDataset !== null &&
-          selectedDataset.slice(0, 3).map((item, index) => (
-            <div key={index} className="p-7">
-              <p>
-                <strong>Category:</strong> {item.category}
-              </p>
-              <p>
-                <strong>Sentiment:</strong> {item.sentiment}
-              </p>
-              <p>
-                <strong>Content:</strong> {item.content}
-              </p>
-            </div>
-          ))}
 
         {/* Dropdown to select the type of visualization - potentially remove or adjust this part if not necessary for the current use case */}
         <div className="p-7">
@@ -90,25 +69,41 @@ const Analysis = () => {
             onChange={handleVisualizationChange}
             value={visualization}
           >
-            <option value="">Choose Data Visualization To Display</option>
-            <option value="Bar Chart">Bar Chart</option>
-            <option>Pie Chart</option>
-            <option>Bubble Chart</option>
+            <option disabled value="">Choose Data Visualization To Display</option>
+            <option value="Stacked Bar Chart">Stacked Bar Chart</option>
+            <option value="Line Chart">Line Chart</option>
+            <option value="Pie Chart">Pie Chart</option>
             {/* Additional visualization options */}
           </select>
         </div>
 
-        {/* Visualization component - Adjust or remove based on your need */}
-        {visualization === "Bar Chart" && (
-          <BarChart width={600} height={300} data={selectedDataset}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="pv" fill="#8884d8" />
-            <Bar dataKey="uv" fill="#82ca9d" />
-          </BarChart>
+        {/* Visualization components */}
+        {visualization === "Stacked Bar Chart" && (
+          <div className="flex justify-center p-7">
+            <div className="card glass shadow-xl w-3/4">
+              <div className="card-body">
+                <SentimentStackedBarChart />
+              </div>
+            </div>
+          </div>
+        )}
+        {visualization === "Line Chart" && (
+          <div className="flex justify-center p-7">
+            <div className="card glass shadow-xl w-3/4">
+              <div className="card-body">
+                <SentimentLineChart />
+              </div>
+            </div>
+          </div>
+        )}
+        {visualization === "Pie Chart" && (
+          <div className="flex justify-center p-7">
+            <div className="card glass shadow-xl">
+              <div className="card-body">
+                <SentimentPieCharts />
+              </div>
+            </div>
+          </div>
         )}
 
         <div className="flex justify-center p-2">
