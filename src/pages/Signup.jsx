a83cafaab6c2e26b40c2c0ec88baf12afa5233e4
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-04-01 23:09:56 -0400

    Refactor Signup component to use custom hook and add loading state

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,17 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Layout from '../components/Layout';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+import Layout from "../components/Layout";
 
 export default function Signup() {
+  const { signUp } = useAuth();
+  const [state, setState] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const [error, setError] = useState("");
+  const [signupSuccess, setSignupSuccess] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false); // State to manage loading status
+
+  const navigate = useNavigate(); // Invoke useNavigate
+
+  function handleForm(e) {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    setIsLoading(true); // Indicate loading status
+
+    if (state.password !== state.passwordConfirm) {
+      console.error("Passwords do not match");
+      setError("Passwords do not match");
+      setIsLoading(false); // Reset loading status
+
+      return;
+    }
+
+    try {
+      await signUp(state.email, state.password, state.fullName);
+      setError("");
+      console.log("Created Account Successfully!");
+      setSignupSuccess(true);
+      // Show success message for a few seconds before redirecting
+      setTimeout(() => {
+        navigate("/"); // Adjust the path as needed
+        setIsLoading(false); // Reset loading status
+      }, 2000); // 3000 ms delay = 3 seconds
+    } catch (err) {
+      console.error(err);
+      setError(err.message);
+      setIsLoading(false); // Ensure to reset loading state in case of error as well
+    }
+  }
 
   return (
     <Layout>
       <div className="flex items-center justify-center h-screen bg-base-300 p-5">
-        <div className="card w-full max-w-md glass shadow-xl -mt-24">
+        <div className="card w-full max-w-md glass shadow-xl">
           <div className="card-body">
             <h1 className="text-4xl font-bold text-center mb-6">New Account</h1>
-
-            <form>
+            {/* Used to show the Error/Success notification to the user */}
+            {error ? (
+              <div className="alert alert-error">
+                <span>Error! {error}</span>
+              </div>
+            ) : (
+              signupSuccess && (
+                <div className="alert alert-success">
+                  <span>Signup Successful, Welcome!</span>
+                </div>
+              )
+            )}
+            <form onSubmit={handleSubmit}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Full Name</span>
@@ -21,6 +85,7 @@ export default function Signup() {
                   name="fullName"
                   placeholder="Full Name"
                   className="input input-bordered input"
+                  onChange={handleForm}
                   required
                 />
               </div>
@@ -34,6 +99,7 @@ export default function Signup() {
                   name="email"
                   placeholder="Email"
                   className="input input-bordered input"
+                  onChange={handleForm}
                   required
                 />
               </div>
@@ -47,6 +113,7 @@ export default function Signup() {
                   name="password"
                   placeholder="Password"
                   className="input input-bordered input"
+                  onChange={handleForm}
                   required
                 />
               </div>
@@ -60,13 +127,22 @@ export default function Signup() {
                   name="passwordConfirm"
                   placeholder="Confirm Password"
                   className="input input-bordered input"
+                  onChange={handleForm}
                   required
                 />
               </div>
 
               <div className="form-control mt-8">
-                <button type="submit" className="btn btn-warning">
-                  Create Account
+                <button
+                  type="submit"
+                  className="btn btn-warning"
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <span className="loading loading-spinner loading-md"></span>
+                  ) : (
+                    "Create Account"
+                  )}
                 </button>
               </div>
               <p className="mt-4">
