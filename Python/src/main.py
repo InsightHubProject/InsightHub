8720d605875235a426da6db2a6ed9d8fd52d1704
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-04-01 23:00:19 -0400

    Refactor tweet analysis and data grouping in main.py

--- Python/src/main.py
+++ Python/src/main.py
@@ -5,17 +5,26 @@ def main():
 
     word = "tesla"
 
-    # Used to fetch tweets from the database
+    # Used to fetch tweets from the database (MongoDB)
     fetcher = TweetFetcher()
     tweets = fetcher.get_tweets(word)
 
     # Used to analyze the tweets
     analyzer = TweetAnalyzer()
+
+    # Used to analyze the tweets
     data = analyzer.analyze_tweets(tweets)
+
+    # Used to convert the date to a datetime object
     data = analyzer.convert_date(data)
+
+    # Used to group the data in a table
     grouped_data = analyzer.group_data_table(data)
+
+    # Used to group the data in a list
     grouped_data_list = analyzer.group_data_list(data)
 
+    # Print the data
     print()
     print(f"-----------------Table of {word} Tweets-----------------")
     print(grouped_data, end="\n\n")
