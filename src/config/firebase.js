392485d2253e78574d8976614f16344981a72df3
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-04-01 23:00:32 -0400

    Add Firebase configuration file

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUoAV8_Zy_h6JK2kFXK--YysHWRk69K0A",
+  authDomain: "insight-hub-5cb75.firebaseapp.com",
+  projectId: "insight-hub-5cb75",
+  storageBucket: "insight-hub-5cb75.appspot.com",
+  messagingSenderId: "442139607429",
+  appId: "1:442139607429:web:7e814463de09a1566e44ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app); // Get the Auth service
+
+export { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile }; // Export the functions
+
+  
