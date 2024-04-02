80ab1832b93928c6ad6938bcf777ddd2a71f9546
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-04-02 13:08:14 -0400

    Refactor Home component and add conditional rendering for signup button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import { useAuth } from "../hooks/useAuth";
+
 import hero_img from "../assets/logo.png";
 
 import Layout from "../components/Layout/Layout";
 
 const Home = () => {
+  const { currentUser } = useAuth(); 
   return (
     <Layout>
-    {/* The padding is smaller when the screen is smal */}
+      {/* The padding is smaller when the screen is smal */}
       <div className="hero py-12 sm:py-16 bg-base-300">
         <div className="hero-content grid md:grid-cols-2 gap-8">
           {/* The image for large screen */}
@@ -17,7 +21,7 @@ const Home = () => {
 
           <div>
             <h1 className="text-4xl sm:text-5xl font-bold leading-normal">
-                Welcome to InsightHub, Your Gateway to Social Media Insights!
+              Welcome to InsightHub, Your Gateway to Social Media Insights!
             </h1>
 
             {/* The image for small screen */}
@@ -26,12 +30,20 @@ const Home = () => {
             </div>
 
             <p className="py-6 text-3xl leading-normal">
-                Analyze Trends with Ease, Understand Sentiments in a Breeze
+              Analyze Trends with Ease, Understand Sentiments in a Breeze
             </p>
 
-            <Link to="/about" className="btn btn-warning w-1/2 text-xl">
+            <div className="flex justify-center gap-2">
+            <Link to="/about" className="btn btn-warning flex-grow text-xl">
               About us
             </Link>
+            {/* Conditional rendering based on isLoggedIn */}
+            {!currentUser && (
+              <Link to="/signup" className="btn btn-warning flex-grow text-xl ml-2">
+                Get Started
+              </Link>
+            )}
+            </div>
           </div>
         </div>
       </div>
