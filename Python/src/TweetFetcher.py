e034fabcbcdd1e7a95f678688dd0b787e262bf45
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-04-19 23:03:47 -0400

    Refactor TweetFetcher class to improve brand name handling and data retrieval

--- Python/src/TweetFetcher.py
+++ Python/src/TweetFetcher.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import re
 
 class TweetFetcher:
     def __init__(self):
@@ -10,14 +11,19 @@ class TweetFetcher:
         self.db = client['insight-hub']
         self.collection = self.db['tweets']
 
-    def get_tweets(self, word):
-        word = word.lower()
+    def get_tweets(self, brand):
+        brand = brand.lower()
+        regex_pattern = r"\b" + re.escape(brand) + r"\b"
+        query = {"content": {"$regex": regex_pattern, "$options": "i"}}
+        result = self.collection.find(query)
+        if result.count() == 0:
+            return None  # Return None if no tweets found
         tweets_list = []
-        for doc in self.collection.find():
-            if word in doc['content'].lower():
-                tweets_dict = {}
-                tweets_dict["id"] = doc['_id']
-                tweets_dict["tweet"] = doc['content']
-                tweets_dict["date"] = doc['date']
-                tweets_list.append(tweets_dict)
+        for doc in result:
+            tweets_dict = {
+                "id": doc['_id'],
+                "tweet": doc['content'],
+                "date": doc['date']
+            }
+            tweets_list.append(tweets_dict)
         return tweets_list
