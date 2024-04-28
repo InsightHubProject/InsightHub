17fb84101698429dae81712ed458e2877be120a5
Ziyad-Aljaser <Ziyad.m.aljaser@gmail.com>
2024-04-27 23:20:29 -0400

    Update PythonAPI.jsx to use HTTPS for API endpoint

--- client/src/utils/PythonAPI.jsx
+++ client/src/utils/PythonAPI.jsx
@@ -1,6 +1,8 @@
 export async function fetchProcessedWord(word) {
   // for local use:
   // fetch("https://your-app.onrender.com/process_word"
+
+  // For production use:
   const response = await fetch("https://your-app.onrender.com/process_word", {
     method: "POST",
     headers: {
